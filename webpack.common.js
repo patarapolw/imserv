9aae481e9169746f8ca6dbc7dc3bd447a7696db1
patarapolw <patarapolw@gmail.com>
2019-02-03 17:31:37 +0700

    correct URL

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "[name].min.js"
+        filename: "[name].bundle.js"
     },
     module: {
         rules: [
